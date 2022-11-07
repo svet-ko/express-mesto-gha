6155e24f40fc7c52f109fb8bcc7ad7cca3036553
svetlana_korneeva <svet-ko@list.ru>
2022-11-07 12:36:04 +0200

    feat: card update validation

--- routes/cards.js
+++ routes/cards.js
@@ -6,10 +6,14 @@ const {
   likeCard,
   dislikeCard,
 } = require('../controllers/cards');
+const {
+  validateCardCreation,
+  validateCardId,
+} = require('../utils/validations');
 
 router.get('/', getCards);
-router.post('/', createCard);
-router.delete('/:cardId', deleteCard);
+router.post('/', validateCardCreation, createCard);
+router.delete('/:cardId', validateCardId, deleteCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
 
--- utils/validations.js
+++ utils/validations.js
@@ -37,3 +37,16 @@ module.exports.validateAvatarUpdate = celebrate({
     avatar: Joi.string().regex(HTTP_REGEX),
   }),
 });
+
+module.exports.validateCardCreation = celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().regex(HTTP_REGEX),
+  }),
+});
+
+module.exports.validateCardId = celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().required().hex().length(24),
+  }),
+});
